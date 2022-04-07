bacdfc55a8933f20b6a69ae373a75166672f2771
Michael Jarvis <mjarvis1997@gmail.com>
2022-04-07 00:57:14 -0400

    initial commit

---
+++ src/routes/Gallery.tsx
@@ -0,0 +1,5 @@
+const Gallery = () => {
+  return <h1>gallery page</h1>;
+}
+
+export default Gallery;
---
+++ src/routes/Manager.tsx
@@ -0,0 +1,5 @@
+const Manager = () => {
+    return <h1>manager page</h1>;
+  }
+  
+  export default Manager;
