7ac0c6112c6b87dcc9a52e16e8b1176863962ecf
Michael Jarvis <mjarvis1997@gmail.com>
2022-05-23 23:12:04 -0400

    AF-13: Ability to add a playlist

--- src/routes/PlaylistsView.tsx
+++ src/routes/PlaylistsView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Playlists from '../components/Playlists/Playlists';
+import AddButton from '../components/AddButton';
 
 import '../globalstyle.module.css';
 import styles from '../globalstyle.module.css';
@@ -10,6 +11,7 @@ export default class PlaylistsView extends React.Component {
       <>
         <h1 className={styles.title}>Playlists</h1>
         <Playlists />
+        <AddButton href="/edit/playlist" />
       </>
     );
   }
