5a209f9f76772923b7cabea78fde040cb31096b2
Dddeepdish <masood.cates@yahoo.com>
2022-05-29 13:56:22 -0400

    Global form styling plus redirects

--- src/Components/Auth/LoginRoute.tsx
+++ src/Components/Auth/LoginRoute.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './LoginRoute.scss';
+import './AuthForm.scss';
 import { useNavigate } from 'react-router-dom';
 
 const LoginRoute = () => {
