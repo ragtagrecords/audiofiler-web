9875578837d4941a62804d4d663d883669e0ae9d
aambrioso01 <40615096+aambrioso01@users.noreply.github.com>
2022-06-07 19:26:14 -0400

    list and button style updates, logo update

--- src/Components/Common/SearchBar/SearchBar.tsx
+++ src/Components/Common/SearchBar/SearchBar.tsx
@@ -4,7 +4,7 @@ import './SearchBar.scss';
 const SearchBar = () => {
   return (
     <div className="searchContainer">
-      <input type="text" name="songName" required />
+      <input type="text" name="songName" autoComplete="off" required />
       <label htmlFor="name" className="labelName">
         <span className="contentName">
           search
